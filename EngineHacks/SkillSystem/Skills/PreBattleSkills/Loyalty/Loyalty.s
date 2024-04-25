aa8563daa0ea4deedb6f291707bc4da3d73cd83a
Alice <raulapablaza2001@gmail.com>
2024-04-25 02:30:33 -0400

    Adding skills
    Added a bunch of skills. Getting Error messages when importing. Need assistance.

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/Loyalty/Loyalty.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/Loyalty/Loyalty.s
@@ -6,8 +6,6 @@ push {r4-r7, lr}
 mov r4, r0 @atkr
 mov r5, r1 @dfdr
 
-
-@has Loyalty
 ldr r0, SkillTester
 mov lr, r0
 mov r0, r4 @Attacker data
@@ -16,7 +14,6 @@ ldr r1, LoyaltyID
 cmp r0, #0
 beq End
 
-@now check for the skill
 ldr r0, AuraSkillCheck
 mov lr, r0
 mov r0, r4 @attacker
@@ -38,36 +35,25 @@ ldr r5,CharData
 sub r2,#0x1
 mul r3,r2
 add r5,r3
-mov r3,#0x4
-ldr r5,[r5,r3]
-ldrb r3,[r5,r3]
-cmp r3,#0x1
-beq Final
-cmp r3,#0x2
-beq Final
-cmp r3,#0x3
-beq Final
-cmp r3,#0x4
-beq Final
-cmp r3,#0x3B
-beq Final
-cmp r3,#0x3C
-beq Final
-b Loop
+ldr r0, [r5] @char
+ldr r0, [r0, #0x28] @char abilities
+ldr r3, [r5,#4] @class
+ldr r3, [r3,#0x28] @class abilities
+orr r0, r3
+mov r3, #0x20
+lsl r3, #8 @0x2000 Is Lord
+tst r0, r3
+beq Loop @Loop until find a Lord, or until no units left
 
-
-Final:
 mov r1, #0x5C
 ldrh r2, [r4, r1]
-add r2, #0x3
+add r2, #0x3 @3 Defense
 strh r2, [r4,r1]
 
 mov r1, #0x60
 ldrh r2, [r4, r1]
-add r2, #15
+add r2, #15 @15 Hit
 strh r2, [r4,r1]
-
-
 
 End:
 pop {r4-r7, r15}
@@ -75,8 +61,6 @@ pop {r4-r7, r15}
 .ltorg
 CharData:
 .long 0x202be4c
-MovementPoin:
-.long 0x880bb96
 SkillTester:
 @Poin SkillTester
 @WORD LoyaltyID
